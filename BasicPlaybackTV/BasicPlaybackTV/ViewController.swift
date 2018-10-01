bcb6d6ce75b753c9228c2d690e10f688d669b82b
Lorenz Schmoliner <lorenz.schmoliner@bitmovin.com>
2018-10-01 16:22:30 +0200

    Enable auto play on BasicPlaybackTV

--- BasicPlaybackTV/BasicPlaybackTV/ViewController.swift
+++ BasicPlaybackTV/BasicPlaybackTV/ViewController.swift
@@ -10,44 +10,47 @@ import UIKit
 import BitmovinPlayer
 
 final class ViewController: UIViewController {
-    
+
     var player: BitmovinPlayer?
-    
+
     deinit {
         player?.destroy()
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         self.view.backgroundColor = .black
-        
+
         // Define needed resources
         guard let streamUrl = URL(string: "https://bitmovin-a.akamaihd.net/content/MI201109210084_1/m3u8s/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8") else {
             return
         }
-        
+
         // Create player configuration
         let config = PlayerConfiguration()
-        
+
         do {
             try config.setSourceItem(url: streamUrl)
-            
+
+            // Enable auto play
+            config.playbackConfiguration.isAutoplayEnabled = true
+
             // Create player based on player configuration
             let player = BitmovinPlayer(configuration: config)
-            
+
             // Create player view and pass the player instance to it
             let playerView = BMPBitmovinPlayerView(player: player, frame: .zero)
 
             // Listen to player events
             player.add(listener: self)
-            
+
             playerView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
             playerView.frame = view.bounds
-            
+
             view.addSubview(playerView)
             view.bringSubview(toFront: playerView)
-            
+
             self.player = player
         } catch {
             print("Configuration error: \(error)")
@@ -56,25 +59,24 @@ final class ViewController: UIViewController {
 }
 
 extension ViewController: PlayerListener {
-    
+
     func onPlay(_ event: PlayEvent) {
         print("onPlay \(event.time)")
     }
-    
+
     func onPaused(_ event: PausedEvent) {
         print("onPaused \(event.time)")
     }
-    
+
     func onTimeChanged(_ event: TimeChangedEvent) {
         print("onTimeChanged \(event.currentTime)")
     }
-    
+
     func onDurationChanged(_ event: DurationChangedEvent) {
         print("onDurationChanged \(event.duration)")
     }
-    
+
     func onError(_ event: ErrorEvent) {
         print("onError \(event.message)")
     }
 }
-
